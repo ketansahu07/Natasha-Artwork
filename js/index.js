be7152254f7af5fcb4e3d9ee733c7d7a2772615d
Ketan Sahu <ketankumar120@gmail.com>
2020-11-25 01:58:43 +0530

    infinite scroll added to the products page

--- js/index.js
+++ js/index.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
     // getting the categories for navbar
     let categories_url = "http://127.0.0.1:8000/api/categories";
-    let categories_data;
-    let get_categories_data = $.get(categories_url, function(data){
-        // console.log(data.results[0])
-        categories_data = data;
-        $.each(data.results, function(index, element) {
+    let categories_set;
+    let get_categories = $.get(categories_url, function(data){
+        // console.log(data.results)
+        categories_set = data.results;
+        $.each(categories_set, function(index, element) {
             let a_tag = $('<a class="nav-link nav_tab" href="#"></a>').attr("name", `${element.title}`).text(element.title);
             let li_tag = $('<li class="nav-item"></li>').append(a_tag);
             $("#categories_list").append(li_tag);
         });
     });
 
-    $.when(get_categories_data).done(function () {
+    $.when(get_categories).done(function () {
         let nav_tabs = $(".nav_tab")
         $.each(nav_tabs, function(index) {
             $(this).click(function () {     // adding click function to each tab
@@ -21,4 +21,5 @@ $(document).ready(function () {
             });
         });
     });
+
 });
--- js/products.js
+++ js/products.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
     // getting the categories for navbar
     let categories_url = "http://127.0.0.1:8000/api/categories";
-    let categories_data;
-    let get_categories_data = $.get(categories_url, function(data){
+    let categories_set;
+    let get_categories = $.get(categories_url, function(data){
         // console.log(data.results[0])
-        categories_data = data;
-        $.each(data.results, function(index, element) {
-            let a_tag = $('<a class="nav-link nav_tab"></a>').attr("href", `products.html?category=${element.title}`).text(element.title);
+        categories_set = data.results;
+        $.each(categories_set, function(index, element) {
+            let a_tag = $('<a class="nav-link nav_tab" href="#"></a>').attr("name", `${element.title}`).text(element.title);
             let li_tag = $('<li class="nav-item"></li>').append(a_tag);
             $("#categories_list").append(li_tag);
         });
     });
 
-    $.when(get_categories_data).done(function () {
+    $.when(get_categories).done(function () {
         let nav_tabs = $(".nav_tab")
         $.each(nav_tabs, function(index) {
             $(this).click(function () {     // adding click function to each tab
@@ -22,12 +22,12 @@ $(document).ready(function() {
         });
     });
 
-    let this_category_name = '';
 
-    let get_this_cat_name = $.when(get_categories_data).done(function() {
+    let this_category_name = '';
+    let get_this_cat_name = $.when(get_categories).done(function() {
         // function to get url for the clicked category -----> currently of no use
         function geturl(name) {     
-            let obj = categories_data.results.find(item => item.title === name);
+            let obj = categories_set.results.find(item => item.title === name);
             return obj.url;
         }
 
@@ -51,39 +51,101 @@ $(document).ready(function() {
         });
     });
 
-    let products_url = "http://127.0.0.1:8000/api/products/";
-    let products_data, product_set;
+
     // creating rows of products and appending them to the container
-    $.when(get_this_cat_name).done(function() {     
+    function createRows(product_set){
+        let item = 0;
+        while(item<product_set.length){
+            let row = $('<div class="row row-content"></div>')
+            let col = $('<div class="col-12"></div>')
+            let flex_div = $('<div class="d-flex flex-wrap justify-content-around"></div>')
+            for(let i=0; i<3; i++){
+                if(item<product_set.length){
+                    let card = $('<div class="card m-2" style="width: 18rem;"></div>');
+                    let card_img = $('<img class="card-img-top" src="../img/product_1.JPG" alt="Card image cap">');
+                    let card_body = $('<div class="card-body"></div>');
+                    let title = $('<h5 class="card-title"></h5>').text(product_set[item].title);
+                    let description = $('<p class="card-text"><small class="text-muted"></p>').text(product_set[item].description);
+                    let price = $('<p class="card-text"></p>').text(`₹${product_set[item].price}`);
+                    card_body.append(title).append(description).append(price)
+                    card.append(card_img).append(card_body)
+                    flex_div.append(card)
+                    item = item + 1;
+                }
+            }
+            col.append(flex_div)
+            row.append(col)
+            $("#rows").append(row)
+            // $("#rows").replaceWith(row)
+        }
+    }
+
+
+    let products_url = "http://127.0.0.1:8000/api/products/";
+    let product_set;
+    let prev_page = null, next_page = null;
+    let get_products = $.when(get_this_cat_name).done(function() {     
         if(this_category_name != ''){
             products_url = `http://127.0.0.1:8000/api/products/?category=${this_category_name}`
         }
         $.get(products_url, {"category": this_category_name}).done(function(data){
             // console.log(data)
-            products_data = data
+            prev_page = data.links.previous
+            next_page = data.links.next
             product_set = data.results
-            let item = 0;
-            while(item<product_set.length){
-                let row = $('<div class="row row-content"></div>')
-                let col = $('<div class="col-12"></div>')
-                let flex_div = $('<div class="d-flex flex-wrap justify-content-around"></div>')
-                for(let i=0; i<3; i++){
-                    if(item<product_set.length){
-                        let card = $('<div class="card m-2" style="width: 18rem;"></div>');
-                        let card_img = $('<img class="card-img-top" src="../img/product_1.JPG" alt="Card image cap">');
-                        let card_body = $('<div class="card-body"></div>');
-                        let title = $('<h5 class="card-title"></h5>').text(product_set[item].title);
-                        let description = $('<p class="card-text"><small class="text-muted"></p>').text(product_set[item].description);
-                        let price = $('<p class="card-text"></p>').text(`₹${product_set[item].price}`);
-                        card_body.append(title).append(description).append(price)
-                        card.append(card_img).append(card_body)
-                        flex_div.append(card)
-                        item = item + 1;
-                    }
+            createRows(product_set)
+        });
+    });
+
+    // $.when(get_products).done(function() {
+    //     $("#prev").click(function(event) {
+    //         if(prev_page != null){
+    //             $.get(prev_page, {"category": this_category_name}).done(function(data){
+    //                 // console.log(data)
+    //                 prev_page = data.links.previous
+    //                 next_page = data.links.next
+    //                 product_set = data.results
+    //                 createRows(product_set)
+    //             });
+    //         }
+    //         else{
+    //             event.preventDefault();
+    //         }
+    //     });
+        
+    //     $("#next").click(function(event) {
+    //         if(next_page != null){
+    //             $.get(next_page, {"category": this_category_name}).done(function(data){
+    //                 // console.log(data)
+    //                 prev_page = data.links.previous
+    //                 next_page = data.links.next
+    //                 product_set = data.results
+    //                 createRows(product_set)
+    //             });
+    //         }
+    //         else{
+    //             event.preventDefault();
+    //         }
+    //     });
+    // });
+
+    // for infinite scrolling 
+
+    $.when(get_products).done(function() {
+        var scrollLoad = true;
+
+        $(window).scroll(function () { 
+            if (scrollLoad && $(window).scrollTop() >= $(document).height() - $(window).height() - 10) {
+                scrollLoad = false;
+                //Add something at the end of the page
+                if(next_page != null){
+                    $.get(next_page, {"category": this_category_name}).done(function(data){
+                        prev_page = data.links.previous
+                        next_page = data.links.next
+                        product_set = data.results
+                        createRows(product_set)
+                    });
                 }
-                col.append(flex_div)
-                row.append(col)
-                $("#rows").append(row)
             }
         });
     });
